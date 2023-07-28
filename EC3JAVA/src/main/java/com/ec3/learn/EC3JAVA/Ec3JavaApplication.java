dae0849288c279540fed696c781cc782dca11b39
Itamarhf <136043045+Itamarhf@users.noreply.github.com>
2023-07-27 23:21:19 -0500

    Add files via upload

---
+++ EC3JAVA/src/main/java/com/ec3/learn/EC3JAVA/Ec3JavaApplication.java
@@ -0,0 +1,13 @@
+package com.ec3.learn.EC3JAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3JavaApplication.class, args);
+	}
+
+}
---
+++ EC3JAVA/src/test/java/com/ec3/learn/EC3JAVA/Ec3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec3.learn.EC3JAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
